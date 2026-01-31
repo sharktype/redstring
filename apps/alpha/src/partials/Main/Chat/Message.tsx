a7e3becd7e477393a33e22281b61cd1cfc726fc7
sharktype <github.avenging156@passmail.net>
2026-01-31 19:09:22 +1100

    Add working edit logic and prep for regenerate

--- apps/alpha/src/partials/Main/Chat/Message.tsx
+++ apps/alpha/src/partials/Main/Chat/Message.tsx
@@ -1,8 +1,9 @@
 import type MessageData from "../../../models/Message.ts";
-import { Badge, Box, Button, Card, type DefaultMantineColor, Flex } from "@mantine/core";
+import { Badge, Box, Button, Card, type DefaultMantineColor, Flex, Textarea } from "@mantine/core";
 import { useLlmContext } from "../../../context/LlmContext.tsx";
 import Markdown from "react-markdown";
 import { useLocalStorage } from "@mantine/hooks";
+import { useState } from "react";
 
 interface MessageProps {
   message: MessageData;
@@ -10,6 +11,11 @@ interface MessageProps {
 
 export default function Message(props: MessageProps) {
   const [messages, setMessages] = useLocalStorage<MessageData[]>({ key: "messages", defaultValue: [] });
+
+  const [temporaryEditMessage, setTemporaryEditMessage] = useState(props.message.content);
+  const [isDeleteConfirm, setIsDeleteConfirm] = useState(false);
+  const [isRegenerateConfirm, setIsRegenerateConfirm] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
 
   let speakerText = "";
   let speakerColor: DefaultMantineColor = "gray";
@@ -25,6 +31,18 @@ export default function Message(props: MessageProps) {
   }
 
   const isUser = props.message.role === "user";
+  const message = isEditMode ? (
+    <Textarea
+      value={temporaryEditMessage}
+      onChange={(e) => setTemporaryEditMessage(e.currentTarget.value)}
+      flex={1}
+      autosize
+    />
+  ) : (
+    <Box maw={780} style={{ overflowX: "auto" }}>
+      <Markdown>{props.message.content}</Markdown>
+    </Box>
+  );
 
   return (
     <Flex mb="md" justify={isUser ? "flex-end" : "flex-start"}>
@@ -32,22 +50,94 @@ export default function Message(props: MessageProps) {
         <Flex mb="xs">
           <Badge color={speakerColor}>{speakerText}</Badge>
         </Flex>
-        <Box maw={780} style={{ overflowX: "auto" }}>
-          <Markdown>{props.message.content}</Markdown>
-        </Box>
-        <Button
-          size="xs"
-          mt="md"
-          color="red"
-          variant="subtle"
-          onClick={() => {
-            // this sucks but whatever
+        {message}
+        <Box>
+          {isEditMode ? (
+            <Button
+              size="xs"
+              mt="md"
+              color="green"
+              variant="subtle"
+              onClick={() => {
+                const updatedMessages: MessageData[] = messages.map((m) => {
+                  if (m.content === props.message.content) {
+                    return { ...m, content: temporaryEditMessage.trim() };
+                  }
 
-            setMessages(messages.filter((m) => m.content !== props.message.content));
-          }}
-        >
-          Delete
-        </Button>
+                  return m;
+                });
+
+                setMessages(updatedMessages);
+                setIsEditMode(false);
+              }}
+            >
+              Save
+            </Button>
+          ) : null}
+          {isEditMode ? (
+            <Button
+              size="xs"
+              mt="md"
+              color="red"
+              variant="subtle"
+              onClick={() => {
+                setIsEditMode(false);
+                setTemporaryEditMessage(props.message.content);
+              }}
+            >
+              Close Edit
+            </Button>
+          ) : (
+            <Button
+              size="xs"
+              mt="md"
+              color="green"
+              variant="subtle"
+              onClick={() => {
+                setIsEditMode(true);
+                setTemporaryEditMessage(props.message.content);
+              }}
+            >
+              Edit
+            </Button>
+          )}
+          {props.message.role !== "user" && (
+            <Button
+              size="xs"
+              mt="md"
+              color="yellow"
+              variant="subtle"
+              onClick={() => {
+                if (isRegenerateConfirm) {
+                  // Regenerate logic
+                } else {
+                  setIsRegenerateConfirm(true);
+                }
+              }}
+            >
+              {isRegenerateConfirm ? "Confirm Regenerate?" : "Regenerate"}
+            </Button>
+          )}
+          {isEditMode ? null : (
+            <Button
+              size="xs"
+              mt="md"
+              color="red"
+              variant="subtle"
+              onClick={() => {
+                // this sucks but whatever
+
+                if (isDeleteConfirm) {
+                  setMessages(messages.filter((m) => m.content !== props.message.content));
+                } else {
+                  setIsDeleteConfirm(true);
+                }
+              }}
+            >
+              {isDeleteConfirm ? "Confirm Delete?" : "Delete"}
+            </Button>
+          )}
+        </Box>
       </Card>
     </Flex>
   );
