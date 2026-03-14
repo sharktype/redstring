99114ce41b607c6abf127670ceba69b16cae7917
sharktype <github.sharktype@proton.me>
2026-03-14 15:38:43 +1100

    Change sidebar to have more structure

--- apps/beta/src/components/partials/sidebar/Status.tsx
+++ apps/beta/src/components/partials/sidebar/Status.tsx
@@ -1,17 +1,43 @@
-import { Box, Stack } from "@mantine/core";
+import { Box, Button, Stack, Text, Flex } from "@mantine/core";
+import type { ReactNode } from "react";
+import { GiBackpack, GiSkills } from "react-icons/gi";
+import { FaScroll, FaMap } from "react-icons/fa";
+import { BsBook } from "react-icons/bs";
 
 export default function Status() {
 	return (
-		<Stack>
-			<Box>
-				Character Portrait (not a real portrait, of course, since this is a text
-				game)
-			</Box>
-			<Box>Character Ephemeral Stats</Box>
-			<Box>Equipped Items</Box>
-			<Box>
-				Buttons to open panels (inventory, quests, transport/map, skills, codex)
-			</Box>
-		</Stack>
+		<Flex direction="column" h="100%">
+			<Stack flex={1}>
+				<Box>
+					<Text>
+						<b>Character Name</b>
+					</Text>
+					<Text size="xs">Level N Race Class (Current/Next XP)</Text>
+				</Box>
+				<Box>Character Major Stats</Box>
+				<Box>Equipped Items</Box>
+			</Stack>
+			<Stack gap="xs">
+				<StatusItem label="Inventory" icon={<GiBackpack />} />
+				<StatusItem label="Quests" icon={<FaScroll />} />
+				<StatusItem label="Map" icon={<FaMap />} />
+				<StatusItem label="Skills" icon={<GiSkills />} />
+				<StatusItem label="Codex" icon={<BsBook />} />
+			</Stack>
+		</Flex>
+	);
+}
+
+function StatusItem(props: { label: string; icon: ReactNode }) {
+	return (
+		<Button
+			variant="default"
+			size="xs"
+			leftSection={props.icon}
+			justify="flex-start"
+			onClick={() => null}
+		>
+			{props.label}
+		</Button>
 	);
 }
